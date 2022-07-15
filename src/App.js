167cd2029783c9c86c0895bebe0c2a19f705b357
Heein Park <gmldls94@gmail.com>
2022-07-15 12:03:57 +0900

    Menu Components
    This will generate a menu with links.
    Items in a prop should be an array.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import './App.css';
 const Home = () => (<h1>Home</h1>)
 const MyPage = () => (<h1>My Page</h1>)
 const Dashboard = () => (<h1>Dashboard</h1>)
+const Menu = (props) => {
+  const items = props.items;
+
+  if (items && Array.isArray(items)) {
+    return (
+        <nav>
+          <ul>
+            {items.map(
+              (item, i) => {
+                const itemName = item.type.name
+                return (
+                  <li key={i}>
+                    <Link to={`${itemName.toLowerCase()}`}>{itemName}</Link>
+                  </li>
+                )
+              }
+            )}
+          </ul>
+        </nav>
+    )
+  } else {
+    return <div></div>
+  }
+}
 
 function App() {
   return (
